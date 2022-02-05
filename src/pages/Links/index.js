c4a4eb167a0ecf0253aef11f6b8370aa3c732d30
WantuilMicael <wantuimicael@hotmail.com>
2022-02-04 21:51:00 -0300

    Estilizando a pagina Meus Links

--- src/pages/Links/index.js
+++ src/pages/Links/index.js
@@ -1,8 +1,38 @@
+import './links.css';
+import { FiArrowLeft, FiLink, FiTrash } from 'react-icons/fi'
+import { Link } from 'react-router-dom'
 
 export default function Links(){
     return(
-      <div>
-        <h1>Pagina Meus Links</h1>
+      <div className="links-container">
+
+        <div className="links-header">
+          <Link to="/">
+            <FiArrowLeft size={38} color="#fff"/>
+          </Link>
+            <h1>Meus Links</h1>
+        </div>
+
+        <div className="links-item">
+            <button className="link">
+              <FiLink sizer={18} color="#fff"/>        
+              https://wantuilmicael.com
+            </button>
+            <button className="link-delete">
+              <FiTrash size={24} color="#ff5454"/>
+            </button>
+        </div>
+
+        <div className="links-item">
+            <button className="link">
+              <FiLink sizer={18} color="#fff"/>
+              https://thecube.com
+            </button>
+            <button className="link-delete">
+              <FiTrash size={24} color="#ff5454"/>
+            </button>
+        </div>
+
       </div>
     )
   }
